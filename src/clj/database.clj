3ed2dc26bde3ffe36a20b77d5bbc7398d68ee697
Dan <dan@eighttrigrams.net>
2023-04-16 19:53:18 +0100

    Add validation

--- src/clj/database.clj
+++ src/clj/database.clj
@@ -1,12 +1,26 @@
 (ns database
-  (:require [xtdb.api :as xt]))
+  (:require [clojure.spec.alpha :as s]
+            [xtdb.api :as xt]))
 
 (defonce node (xt/start-node {}))
 
-(defn put-seed-variety [data]
+(s/def ::name string?)
+(s/def ::type string?)
+(s/def ::stability string?)
+(s/def ::manufacturer string?)
+
+(s/def ::data (s/keys :req-un [::name ::type ::stability ::manufacturer]))
+
+(defn put-seed-instance [data] 
+  (when-not (s/valid? ::data data)
+    (throw (Exception. "not valid"))) 
   (let [uuid (.toString (java.util.UUID/randomUUID))]
     (xt/submit-tx node 
                   [[::xt/put (merge {:xt/id uuid}
                                     data)]])
     (xt/sync node)
     uuid))
+
+(defn get-seed-instance-by-id [id]
+  (xt/entity (xt/db node) id))
+
